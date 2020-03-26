60384c93da65eb865e0b31cb6441e37fcdedb519
ilsubyeega <ilsubyeega@naver.com>
2020-03-26 15:22:46 +0900

    add Packet, PacketParser with wip

---
+++ Sakamoto/Objects/RawPacket.cs
@@ -0,0 +1,28 @@
+﻿using Sakamoto.Enums;
+
+namespace Sakamoto.Objects
+{
+	class RawPacket
+	{
+		public RawPacket(short type, int length, byte[] bytearray)
+		{
+			this.type = type;
+			this.length = length;
+			this.bytearray = bytearray;
+		}
+
+		public short type;
+		public int length;
+		public byte[] bytearray;
+
+		public PacketType getType()
+		{
+			return (PacketType)type;
+		}
+
+		public string toString()
+		{
+			return "Type: " + (PacketType)type + " Length: " + length;
+		}
+	}
+}
---
+++ Sakamoto/Packet/Parser/RawByteParser.cs
@@ -0,0 +1,31 @@
+﻿using osu.Shared.Serialization;
+using Sakamoto.Objects;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sakamoto.Packet.Parser
+{
+	class RawByteParser
+	{
+		public static List<RawPacket> Parse(byte[] bytearray)
+		{
+			List<RawPacket> list = new List<RawPacket>();
+			try
+			{
+				var stream = new MemoryStream(bytearray);
+				SerializationReader r = new SerializationReader(stream);
+				while (r.BaseStream.Position != r.BaseStream.Length)
+				{
+					short type = r.ReadInt16();
+					int length = r.ReadInt32();
+					list.Add(new RawPacket(type, length, r.ReadBytes(length)));
+				}
+				return list;
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
