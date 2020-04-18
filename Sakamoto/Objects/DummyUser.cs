ed918bc27df26bc292112fabfa3169f97785ce3d
ilsubyeega <ilsubyeega@naver.com>
2020-04-18 20:19:13 +0900

    create DummyUser.cs

---
+++ Sakamoto/Objects/DummyUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sakamoto.Objects
+{
+	public class DummyUser
+	{
+		public int id;
+		public string username;
+		public string username_safe;
+		public bool is_restricted;
+		public UserGame status = new UserGame();
+	}
+}
