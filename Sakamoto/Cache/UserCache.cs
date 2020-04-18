de99f57d3e19a69fae09c8ed972311a9401b3897
ilsubyeega <ilsubyeega@naver.com>
2020-04-18 20:19:57 +0900

    add cache wip

--- Sakamoto/Cache/UserCache.cs
+++ Sakamoto/Cache/UserCache.cs
@@ -1,6 +1,11 @@
-﻿namespace Sakamoto.Cache
+﻿using Sakamoto.Objects;
+using System.Collections.Generic;
+
+namespace Sakamoto.Cache
 {
 	public static class UserCache
 	{
+		// This list is online lists.
+		public static List<User> userlist;
 	}
 }
