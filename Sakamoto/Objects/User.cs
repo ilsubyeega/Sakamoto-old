af10da7215e67491d8fabc0af5e8ffb48d543894
ilsubyeega <ilsubyeega@naver.com>
2020-04-18 20:19:22 +0900

    add is_restricted to User

--- Sakamoto/Objects/User.cs
+++ Sakamoto/Objects/User.cs
@@ -23,6 +23,8 @@ namespace Sakamoto.Objects
 		public bool is_donor;
 		public int donor_expire;
 
+		public bool is_restricted;
+
 		public BanchoUserStatus status = new BanchoUserStatus();
 		public UserGame pp = new UserGame();
 
@@ -43,6 +45,32 @@ namespace Sakamoto.Objects
 
 	public class UserGame
 	{
+		public UserGame()
+		{
+
+		}
+		public UserGame(int pp_osu, int pp_osutaiko, int pp_osucatch, int pp_osumania, 
+			int pp_osu_relax, int pp_osutaiko_relax, int pp_osucatch_relax, 
+			int playcount_osu, int playcount_osutaiko, int playcount_osucatch, int playcount_osumania, 
+			int playcount_osu_relax, int playcount_osutaiko_relax, int playcount_osucatch_relax)
+		{
+			this.pp_osu = pp_osu;
+			this.pp_osutaiko = pp_osutaiko;
+			this.pp_osucatch = pp_osucatch;
+			this.pp_osumania = pp_osumania;
+			this.pp_osu_relax = pp_osu_relax;
+			this.pp_osutaiko_relax = pp_osutaiko_relax;
+			this.pp_osucatch_relax = pp_osucatch_relax;
+			this.playcount_osu = playcount_osu;
+			this.playcount_osutaiko = playcount_osutaiko;
+			this.playcount_osucatch = playcount_osucatch;
+			this.playcount_osumania = playcount_osumania;
+			this.playcount_osu_relax = playcount_osu_relax;
+			this.playcount_osutaiko_relax = playcount_osutaiko_relax;
+			this.playcount_osucatch_relax = playcount_osucatch_relax;
+		}
+
+
 		public int pp_osu;
 		public int pp_osutaiko;
 		public int pp_osucatch;
