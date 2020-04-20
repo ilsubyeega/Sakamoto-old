1f7e31dfe8a5c51b85fff30fad27841e46007a1d
ilsubyeega <ilsubyeega@naver.com>
2020-04-20 21:26:54 +0900

    add Common (build_date)

---
+++ Sakamoto/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Sakamoto
+{
+	public static class Common
+	{
+		public static DateTime build_date = new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+	}
+}
