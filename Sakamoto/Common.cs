ca5d1c438d2fac8ca838b088177a68ebeb5d3b4b
ilsubyeega <ilsubyeega@naver.com>
2020-04-22 00:44:51 +0900

    add some common things

--- Sakamoto/Common.cs
+++ Sakamoto/Common.cs
@@ -7,5 +7,13 @@ namespace Sakamoto
 	public static class Common
 	{
 		public static DateTime build_date = new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+
+		public static string bot_name = "Sakamoto";
+		public static int bot_userid = 3;
+
+		public static string osustring = "https://osu.ppy.sh/";
+		public static string path_data = "Data";
+		public static string path_beatmaps = Path.Combine(path_data, "Beatmaps");
+		public static string path_replays = Path.Combine(path_data, "Replays");
 	}
 }
