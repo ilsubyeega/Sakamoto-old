1025c13132e864cb5af647cbdaf919e2122d2a34
ilsubyeega <ilsubyeega@naver.com>
2020-04-22 00:47:53 +0900

    disable warning

--- Sakamoto/Controllers/MainController.cs
+++ Sakamoto/Controllers/MainController.cs
@@ -8,7 +8,9 @@ namespace Sakamoto.Controllers
 	public class MainController : Controller
 	{
 		[HttpGet]
+#pragma warning disable CS1998 // 이 비동기 메서드에는 'await' 연산자가 없으며 메서드가 동시에 실행됩니다.
 		public async Task<IActionResult> Get()
+#pragma warning restore CS1998 // 이 비동기 메서드에는 'await' 연산자가 없으며 메서드가 동시에 실행됩니다.
 		{
 			return Ok("Sakamoto (Bancho) \nosu!bancho reversing project");
 		}
