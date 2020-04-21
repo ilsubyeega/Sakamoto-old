80341dc16de34234ad54062bea3a545ad2b441af
ilsubyeega <ilsubyeega@naver.com>
2020-04-22 00:49:12 +0900

    Add Sakamoto.Test for calculation.

--- Sakamoto.Test/Program.cs
+++ Sakamoto.Test/Program.cs
@@ -1,36 +1,17 @@
 ﻿using System;
 using System.IO;
 using osu.Shared.Serialization;
+using Sakamoto.Test.Tests;
+
 namespace Sakamoto.Test
 {
 	class Program
 	{
-		// Add packet array below (Using fiddler Inspectors -> Response -> HexView -> Copy -> Copy as 0x##)
-		static byte[] arrOutput = {  };
-		static string a = "a";
 		static void Main(string[] args)
 		{
-			Test(a);
-			Console.WriteLine(a);
+			Calculate.Init();
+			Console.ReadLine();
 		}
-		static void Test(string b)
-		{
-			b = "owo";
-		}
-		static void Parse()
-		{
-			var stream = new MemoryStream(arrOutput);
-			SerializationReader r = new SerializationReader(stream);
-			while (r.BaseStream.Position != r.BaseStream.Length)
-			{
-				Console.WriteLine("Type: " + (PacketType)r.ReadInt16());
-				r.ReadByte();
-
-				int length = r.ReadInt32();
-				Console.WriteLine("Length: " + length);
-				r.ReadBytes(length);
-				//Console.WriteLine("Data: " + r.ReadBytes(length).ToString());
-			}
-		}
+		
 	}
 }
---
+++ Sakamoto.Test/Tests/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using osu.Game.Rulesets.Scoring;
+using Sakamoto.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Sakamoto.Test.Tests
+{
+	public static class Calculate
+	{
+		public static int beatmapid = 1764213; // harumachi clover
+		public static void Init()
+		{
+			Sakamoto.Common.path_beatmaps = Path.Combine(@"C:\Users\ilsubyeega\Documents", "Beatmaps");
+			CalcDiff(); // 5.52*
+			CalcPP(); // Accroding to StreamCompanion = 197
+		}
+		public static void CalcPP()
+		{
+			Dictionary<HitResult, int> stats = new Dictionary<HitResult, int>
+				{
+					{ HitResult.Perfect, 47 }, // geki
+					{ HitResult.Great, 116 }, // 300
+					{ HitResult.Good, 0 }, // 100
+					{ HitResult.Ok, 0 }, // katu
+					{ HitResult.Meh, 0 }, // 50
+					{ HitResult.Miss, 0 } // miss
+				};
+			Console.WriteLine("Performance Calculation Result: {0}", Sakamoto.Util.Osu.PerformanceCalculator.Calculate(1537198, 0, (int)Mods.DoubleTime, 151, stats));
+		}
+		public static void CalcDiff()
+		{
+			Console.WriteLine("Diffculty Calculation Result: {0}", Sakamoto.Util.Osu.DifficultyCalculator.Calculate(beatmapid, 0, 0));
+		}
+	}
+}
