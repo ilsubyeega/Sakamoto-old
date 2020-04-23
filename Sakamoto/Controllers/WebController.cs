966af4fa2f316a574747c78862ef5c677b998c6d
ilsubyeega <ilsubyeega@naver.com>
2020-04-23 18:28:00 +0900

    bump

---
+++ Sakamoto/Controllers/WebController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using osu.Shared;
+using Sakamoto.Cache;
+using Sakamoto.Objects.InGame;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sakamoto.Controllers
+{
+
+	[Route("/web/")]
+	public class WebController : Controller
+	{
+		[HttpGet("")]
+		public IActionResult i()
+		{
+			return Ok("Pog");
+		}
+		[HttpGet("osu-osz2-getscores.php")]
+		public async Task<IActionResult> GetScores(
+			[FromQuery(Name = "v")] int type,
+			[FromQuery(Name = "c")] string md5,
+			[FromQuery(Name = "f")] string fileName,
+			[FromQuery(Name = "i")] int beatmapSetId,
+			[FromQuery(Name = "m")] GameMode playMode,
+			[FromQuery(Name = "mods")] Mods mods,
+			[FromQuery(Name = "us")] string username,
+			[FromQuery(Name = "ha")] string password,
+			[FromQuery(Name = "v")] int scoreboard_type,
+			[FromQuery(Name = "vv")] int scoreboard_version)
+		{
+			Scoreboard s = ScoreboardCache.GetScoreboard((int)playMode, beatmapSetId, md5);
+			return Ok(s.AppendToString());
+		}
+	}
+}
--- Sakamoto/Objects/InGame/Beatmap.cs
+++ Sakamoto/Objects/InGame/Beatmap.cs
@@ -14,6 +14,7 @@ namespace Sakamoto.Objects.InGame
 		public string artist;
 		public string creator;
 		public string diff;
+		public GameMode mode;
 
 		public string file_md5;
 		public string filename;
@@ -25,6 +26,9 @@ namespace Sakamoto.Objects.InGame
 		public int totalscores = 0;
 		public int offset = 0;
 		public double rating = 10.0;
+
+		public bool hasvideo;
+
 		public Beatmap() { }
 		public Beatmap(String input)
 		{
@@ -51,6 +55,14 @@ namespace Sakamoto.Objects.InGame
 				a += $"|{beatmapid}|{beatmapsetid}|{totalscores}\n{offset}\n{artist} - {title} [{diff}]\n{rating}\n";
 			}
 			return a;
+		}
+		public string toDirect()
+		{
+			return $"{beatmapsetid}.osz|{artist}|{title}|{creator}|{(int)rankedstatus}|10.00|{Common.timestring}|{beatmapsetid}|{beatmapsetid}|{(hasvideo == true ? 1 : 0)}|1|1|7331";
+		}
+		public string toDirectDiff()
+		{
+			return $"{diff}@{(int)mode}";
 		}
 	}
 }
