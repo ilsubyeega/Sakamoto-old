31b75d1cd150756a84b2d1978f23276be79e329e
ilsubyeega <ilsubyeega@gmail.com>
2021-01-09 21:20:57 +0900

    Add ReflectionUtil

---
+++ Sakamoto/Util/ReflectionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Sakamoto.Util
+{
+	public static class ReflectionUtil
+	{
+		public static void SetField(this object obj, string key, object value)
+		{
+			var mfield = obj.GetType().GetField(key, BindingFlags.NonPublic | BindingFlags.Instance);
+			mfield.SetValue(obj, value);
+		}
+	}
+}
