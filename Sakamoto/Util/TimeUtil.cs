c052c57985bceacbb2a6452bb30ef3f2743ab29c
ilsubyeega <ilsubyeega@gmail.com>
2021-01-09 21:21:24 +0900

    Add TimeUtil

---
+++ Sakamoto/Util/TimeUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sakamoto.Util
+{
+	public static class TimeUtil
+	{
+		public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+		{
+			System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+			dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+			return dtDateTime;
+		}
+	}
+}
