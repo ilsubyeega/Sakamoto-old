cbc88e27aa7d0f06a6694ede39c2343a32cde9d3
ilsubyeega <ilsubyeega@naver.com>
2021-05-02 01:06:31 +0900

    Add priority of /api/v2 endpoint

---
+++ Sakamoto/Controllers/CommonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sakamoto.Controllers
+{
+	[ApiController]
+	public class CommonController : ControllerBase
+	{
+		[Route("/", Order = -99)] // in case of
+		[Route("/api/v2", Order = -99)]
+		public IActionResult V2Index()
+		{
+			return Ok("Sakamoto!");
+		}
+	}
+}
